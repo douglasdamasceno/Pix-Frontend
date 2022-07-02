577ecc59a00ed0d3f8a18aff62aa8be1edecf1b3
Douglas Damasceno <douglasmdamasceno@gmail.com>
2022-07-02 17:28:04 -0300

    feat: finished the component success qrcode

--- src/components/dashboard/QRCode/index.tsx
+++ src/components/dashboard/QRCode/index.tsx
@@ -6,10 +6,14 @@ import * as Styles from './styles'
 export default function QRCode() {
   return (
       <Styles.Container>
-           <Styles.Title>QRCode</Styles.Title>
-          <Styles.Message>QRCode Gerado!</Styles.Message>
-          <Styles.ImgQRCode src={IconQRCode} alt="QRCode" />
-          <CopyClipBoard />
+          <Styles.Header>
+            <Styles.Title>QRCode</Styles.Title>
+            <Styles.Message>QRCode Gerado!</Styles.Message>
+          </Styles.Header>  
+          <div id="copyClip">    
+            <Styles.ImgQRCode src={IconQRCode} alt="QRCode" />
+            <CopyClipBoard />
+          </div>
           <Button
               primary
               width="563px"
--- src/components/dashboard/QRCode/styles.ts
+++ src/components/dashboard/QRCode/styles.ts
@@ -5,6 +5,23 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 39px 29px;
+
+  #copyClip {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 89px;
+    margin-bottom: 88px;
+    gap: 30px;
+  }
+`;
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  width: 100%;
 `;
 
 export const Title = styled.h1`
@@ -17,14 +34,15 @@ export const Title = styled.h1`
 
   color: #bdbdbd;
 `;
-
-export const ImgQRCode = styled.img`
-  /* margin-top: 91px;
-  margin-bottom: 66px; */
-`;
 export const Message = styled.h1`
   font-weight: 700;
   font-size: 2.5rem;
   line-height: 3rem;
   color: #424242;
+`;
+
+export const ImgQRCode = styled.img`
+  width: 253px;
+  height: 250px;
+  border-radius: 11px;
 `;
